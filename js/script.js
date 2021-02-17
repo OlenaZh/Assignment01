cdf4cd4ef603102d6721da88b37ee06a445acaa3
OlenaZh <alyona.mesh@gmail.com>
2021-02-17 15:35:34 -0800

    Converting to Camel Case

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// STEP 1 Converting identifiers to Camel Case notation
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction
+
+
+
+// STEP 2
+
+
+// STEP 3
